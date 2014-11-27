2839437144c10b0b455f8431a3ee0ad0bd38a89e
Edison Otro <edi-arango@hotmail.com>
2014-11-26 23:20:25 -0500

    Marquee funcionando en móvil

---
+++ js/Aplicacion.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Comment
+ */
+function verificarCambio() {
+    var cambio=document.getElementById("cambio");
+    if (cambio.textContent.substring(0,1)==="-") {
+        cambio.className="assertive";
+    }
+    
+}
--- js/controllers.js
+++ js/controllers.js
@@ -42,15 +42,67 @@ angular.module('starter.controllers', [])
     { title: 'Rap', id: 5 },
     { title: 'Cowbell', id: 6 }
   ];
+  
+  $scope.loadRoster=function(classlist){
+    $scope.selectedClass=classlist;
+    $scope.activeClass=classlist.class_id;
+  };
 })
 
-.controller('EmpresasCtrl', function($scope) {
-  $scope.empresas = [
-    { sigla: 'GOOG', nombre: 'Google Inc.',cotizacion:'575.5',cambio:'+37.3(0.5%)' },
-    { sigla: 'FB', nombre: 'Facebook Inc.',cotizacion:'575.5',cambio:'+37.3(0.5%)' },
-    { sigla: 'AAPL', nombre: 'Apple Inc.',cotizacion:'575.5',cambio:'+37.3(0.5%)' },
-  ];
-})
+//.controller('EmpresasCtrl', function($scope) {
+//  $scope.empresas = empresas;
+//})
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
+
+
+function EmpresasCtrl($scope, /*$http,*/ $interval) {
+    $scope.empresas = empresas;
+    $scope.empresaTop = empresaTop;
+    $scope.news = [];
+    $scope.conf = {
+        news_length: false,
+        news_pos: 200, // the starting position from the right in the news container
+        news_margin: 20,
+        news_move_flag: true
+    };
+    
+    $scope.init = function() {
+        /*$http.post('the_news_file.json', null).success(function(data) {
+            if (data && data.length > 0) {*/
+                $scope.news = empresas;
+                $interval($scope.news_move ,2);
+        /*  }
+        });*/
+    };
+    
+    $scope.get_news_right = function(idx) {
+        var $right = $scope.conf.news_pos;
+        for (var ri=0; ri < idx; ri++) {
+            if (document.getElementById('news_'+ri)) {
+                $right += $scope.conf.news_margin + angular.element(document.getElementById('news_'+ri))[0].offsetWidth;
+            }
+        }
+        return $right+'px';
+    };
+    
+    $scope.news_move = function() {
+        if ($scope.conf.news_move_flag) {
+            $scope.conf.news_pos--;
+            if ( angular.element(document.getElementById('news_0'))[0].offsetLeft > angular.element(document.getElementById('news_strip'))[0].offsetWidth + $scope.conf.news_margin ) {
+                var first_new = $scope.news[0];
+                $scope.news.push(first_new);
+                $scope.news.shift();
+                $scope.conf.news_pos += angular.element(document.getElementById('news_0'))[0].offsetWidth + $scope.conf.news_margin;
+            }
+        }
+    };
+}
+
+var empresaTop = {sigla: 'GOOG', nombre: 'Google Inc.',cotizacion:'575.5',cambio:'+37.3(1.3%)'};
+var empresas = [
+    { sigla: 'GOOG', nombre: 'Google Inc.',cotizacion:'575.5',cambio:'+37.3(1.3%)' },
+    { sigla: 'FB', nombre: 'Facebook Inc.',cotizacion:'78.4',cambio:'-4.3(0.5%)' },
+    { sigla: 'AAPL', nombre: 'Apple Inc.',cotizacion:'321.4',cambio:'+0.48(0.2%)' },
+  ];
