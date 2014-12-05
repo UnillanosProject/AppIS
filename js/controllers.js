aef76b321be4ed418529345a8abe23b619d640a6
juanseca94 <juanseca_94@hotmail.com>
2014-12-05 02:19:27 -0500

    tendencia conflicto con redirigir...

--- js/controllers.js
+++ js/controllers.js
@@ -563,14 +563,7 @@ $scope.enviarCorreo = function (correo) {
   };
 })
 .controller('PredictionCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+
 })
 
 .controller('ViabilityCtrl', function($scope, $stateParams) {
